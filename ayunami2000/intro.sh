4342971962c34ac365718ff19340c0432b2adb56
Sam Williams <spwilliamsiam@gmail.com>
2021-05-11 14:17:00 -0400

    bruh

--- ayunami2000/intro.sh
+++ ayunami2000/intro.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo loading desktop...
-apt update &> /dev/null
-wget -O virtualgl.deb https://sourceforge.net/projects/virtualgl/files/2.6.5/virtualgl_2.6.5_amd64.deb/download &> /dev/null
-echo "" | apt install -y ./virtualgl.deb xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri python2 fluxbox xterm &> /dev/null
-rm -rf ./virtualgl.deb &> /dev/null
-git clone https://github.com/ayunami2000/noVNC &> /dev/null
+apt update
+wget -O virtualgl.deb https://sourceforge.net/projects/virtualgl/files/2.6.5/virtualgl_2.6.5_amd64.deb/download
+echo "" | apt install -y ./virtualgl.deb xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri python2 fluxbox xterm
+rm -rf ./virtualgl.deb
+git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 6080 &> /dev/null &
 Xvfb -screen 0 900x720x24+32 +extension GLX &> /dev/null &
-export DISPLAY=:0 &> /dev/null
+export DISPLAY=:0
 fluxbox &> /dev/null &
 x11vnc -noshm -geometry 900x720 -shared -forever &> /dev/null &
-echo desktop loaded!
+clear;echo desktop loaded!
