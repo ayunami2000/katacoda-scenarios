04add6fe78b11a19e4d43333300092875cd6609d
Sam Williams <spwilliamsiam@gmail.com>
2021-05-11 13:00:51 -0400

    bruh

--- ayunami2000/intro.sh
+++ ayunami2000/intro.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt update
 wget -O virtualgl.deb https://sourceforge.net/projects/virtualgl/files/2.6.5/virtualgl_2.6.5_amd64.deb/download
-apt install -y ./virtualgl.deb
-apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri python2 fluxbox xterm
+apt install -y ./virtualgl.deb xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri python2 fluxbox xterm git
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 6080 &
 Xvfb -screen 0 900x720x24+32 +extension GLX &
