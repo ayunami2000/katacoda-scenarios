16e67fef9f4bca86f2e4779ea061ea332f86aae3
Sam Williams <spwilliamsiam@gmail.com>
2021-05-11 12:39:35 -0400

    bruh

---
+++ ayunami2000/intro.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt update
+wget -O virtualgl.deb https://sourceforge.net/projects/virtualgl/files/2.6.5/virtualgl_2.6.5_amd64.deb/download
+apt install -y ./virtualgl.deb
+apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri python2 fluxbox xterm
+git clone https://github.com/ayunami2000/noVNC
+./noVNC/utils/launch.sh --listen 6080 &
+Xvfb -screen 0 900x720x24+32 +extension GLX &
+export DISPLAY=:0
+fluxbox &
+x11vnc -noshm -geometry 900x720 -shared -forever &
+rm -- "$0"
