cde12feab5733b064b4c4aa6c16312ecc1f85e81
Sam Williams <spwilliamsiam@gmail.com>
2021-08-07 19:52:14 -0400

    fix portablemc

--- ayunami2000/assets/portablemc.sh
+++ ayunami2000/assets/portablemc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 apt install -y openjdk-8-jre
 cd /root/
-if [ ! -d "/root/portablemc" ]; then
-    git clone https://github.com/mindstorm38/portablemc
+if [ ! -f "/root/portablemc.py" ]; then
+    wget -O portablemc.zip https://github.com/mindstorm38/portablemc/releases/download/v2.0.0/portablemc-single-2.0.0.zip
+    unzip portablemc.zip
+    rm -rf portablemc.zip
 fi
-yes y | python3 /root/portablemc/portablemc.py start -u katacoda
+yes y | python3 /root/portablemc.py start -u katacoda
